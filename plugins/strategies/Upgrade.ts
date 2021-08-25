0a3e853c58c9a9bb99c9fe0919e5257b2e3e1e84
Craig Morris <craig.michael.morris@gmail.com>
2021-08-25 09:03:31 +0100

    only upgrade once we have energy to avoid low on the s curve

--- plugins/strategies/Upgrade.ts
+++ plugins/strategies/Upgrade.ts
@@ -1,7 +1,7 @@
 import GameManager from '../../declarations/src/Backend/GameLogic/GameManager'
 import GameUIManager from '../../declarations/src/Backend/GameLogic/GameUIManager'
 import { LocationId, PlanetLevel, PlanetType, UpgradeBranchName } from '@darkforest_eth/types';
-import { canPlanetUpgrade, getMyPlanets, getPlanetRank, getPlanetRankForBranch } from '../utils'
+import { canPlanetUpgrade, energy, getMyPlanets, getPlanetRank, getPlanetRankForBranch } from '../utils'
 
 declare const df: GameManager
 declare const ui: GameUIManager
@@ -20,6 +20,7 @@ export function upgrade(config: config)
   getMyPlanets()
     .filter(canPlanetUpgrade)
     .filter(p => p.unconfirmedUpgrades.length === 0)
+    .filter(p => energy(p) > 15) // once we upgrade, energy cap goes up which means we're lower on the S curve, better off to wait a bit.
     .filter(p => ! config.fromId || p.locationId === config.fromId)
     .map(p => {
       const range = p.upgradeState[Branches.RANGE]
